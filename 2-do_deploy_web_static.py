c91dc84fbec5a6da4b6455bd980af9c22b4ff8ba
ngatia-nancy <111184362+ngatia-nancy@users.noreply.github.com>
2023-05-19 22:09:16 +0300

    Deploying archives to my web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -49,7 +49,7 @@ web_static_{}/web_static'
                 # re-establish symbolic link
                 run('sudo ln -s /data/web_static/releases/\
 web_static_{}/ /data/web_static/current'.format(timestamp))
-            print("New version deployed!")
+            
         except:
                 return False
 
