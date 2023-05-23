9c0669053f50e859b431a1949d10e3e4e8a5dbec
ngatia-nancy <111184362+ngatia-nancy@users.noreply.github.com>
2023-05-23 17:44:55 +0300

    script that starts a flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     """Displays a string"""
-    return "Hello HBNB! "
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
