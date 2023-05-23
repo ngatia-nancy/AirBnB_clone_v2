4da7266c058ed0ebe5495d7cc0de765675730ae6
ngatia-nancy <111184362+ngatia-nancy@users.noreply.github.com>
2023-05-23 20:52:01 +0300

    used a converter to specify the type of the argument

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Starts a flask web application
+web application must be listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Displays a string"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays a string"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def croute(text):
+    """Displays value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def proute(text="is cool"):
+    """Displays value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Displays the number, which is an integer"""
+    if isinstance(n, int):
+        return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
