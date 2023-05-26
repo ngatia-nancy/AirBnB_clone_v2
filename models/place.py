d80c5a69c10c61e06b514d7d26ebf742df689927
ngatia-nancy <111184362+ngatia-nancy@users.noreply.github.com>
2023-05-27 01:17:51 +0300

    updates

--- models/place.py
+++ models/place.py
@@ -35,7 +35,7 @@ class Place(BaseModel, Base):
         amenity_ids: list of Amenity ids
     """
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    city_id = Column(String(60, collation='latin1_swedish_ci'), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
